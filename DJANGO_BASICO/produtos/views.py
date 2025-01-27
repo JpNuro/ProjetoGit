35e8f0c2963354089bb531502775ece992fbca26
JOAO PEDRO NUNES RODRIGUES <04917895057@senacrs.edu.br>
2025-01-27 17:29:01 -0300

    models

--- DJANGO_BASICO/produtos/views.py
+++ DJANGO_BASICO/produtos/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pessoa
 
 def ver_produto(request):
-    nome = 'Caio'
-    return render(request, 'ver_produto.html', {'nome': nome})
+    if request.method == 'GET':
+        nome = 'Caio'
+        return render(request, 'ver_produto.html', {'nome': nome})
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
+        return HttpResponse(f'Nome: {nome} <br> Idade: {idade}')
 
 def inserir_produto(request):
     return render(request, 'inserir_produto.html')
